b9f6d4bcbf06a28ce48a97104b7551aa1b3228ce
sim <32626992+simn03@users.noreply.github.com>
2026-08-03 16:21:46 -0700

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.py
@@ -0,0 +1,28 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        
+        l = 0
+        r = 1
+
+        area = 0
+        n = len(height)
+
+        while l < r < n:
+            
+            if height[l] <= height[r]:
+                l += 1
+                r = l + 1
+                continue;
+
+            localArea = 0
+            
+            while r < n and height[l] > height[r]:
+                localArea += height[l] - height[r]
+                r += 1
+            
+            if r < n and height[l] <= height[r]:
+                area += localArea
+                l = r
+                r = l + 1
+
+        return area
