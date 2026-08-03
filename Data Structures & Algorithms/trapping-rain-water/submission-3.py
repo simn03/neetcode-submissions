418fdc2f8a58a1248408fb463741937464ec915d
sim <32626992+simn03@users.noreply.github.com>
2026-08-03 16:50:56 -0700

    Add: trapping-rain-water - submission-3

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-3.py
@@ -0,0 +1,28 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        
+        prefix = [0] * len(height)
+        postfix = [0] * len (height)
+
+
+        for i in range(len(height)):
+            preIdx = i
+            postIdx = len(height) - 1 - i
+
+            if (preIdx > 0):
+                prefix[preIdx] = max(height[preIdx - 1], prefix[preIdx - 1])
+
+            if (postIdx < len(height) - 1):
+                postfix[postIdx] = max(height[postIdx + 1], postfix[postIdx + 1])
+
+        area = 0
+
+        for i, h in enumerate(height):
+            wallHeight = min(prefix[i], postfix[i])
+
+            if wallHeight < h:
+                continue
+
+            area += wallHeight  - h
+
+        return area
