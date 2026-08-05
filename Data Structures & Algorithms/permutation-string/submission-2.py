1c880f5e44b248e96cc5c2f6b37dcc7fb101e599
sim <32626992+simn03@users.noreply.github.com>
2026-08-04 21:28:29 -0700

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.py
@@ -0,0 +1,41 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+
+
+        if len(s1) > len(s2):
+            return False
+
+        window_size = len(s1)
+
+        chars = defaultdict(int)
+
+        for c in s1:
+            chars[c] += 1
+
+        freqs = defaultdict(int)
+
+        for i in range(window_size):
+            c = s2[i]
+            freqs[c] += 1
+
+        def isPerm():
+            for k, v in chars.items():
+                if v != freqs[k]:
+                    return False
+
+            return True
+
+        for i in range(len(s2) - window_size + 1):
+            if isPerm():
+                return True
+            
+            char_start = s2[i]
+            freqs[char_start] -= 1
+
+            if i + window_size < len(s2):
+                char_end = s2[i + window_size]
+                freqs[char_end] += 1
+
+        return False
+
+
