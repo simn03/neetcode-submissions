b3acb0b8acd23ea0c60844d2cc45f403186d9c49
sim <32626992+simn03@users.noreply.github.com>
2026-08-07 11:41:24 -0700

    Add: car-fleet - submission-4

---
+++ Data Structures & Algorithms/car-fleet/submission-4.py
@@ -0,0 +1,44 @@
+from collections import namedtuple
+
+Item = namedtuple('Item', ['p', 't'])
+
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        n = len(position)
+
+        stack: list[Item] = []
+
+        for i in range(0, n):
+            p = position[i]
+            d = target - p
+            t = d / speed[i]
+
+            behind: list[Item] = []
+
+            while stack:
+                top = stack[-1]
+
+                # print(f"Item(p={p}, t={t})   =>   {top}")
+
+                if p < top.p and t <= top.t:
+                    # print(f"  Behind and faster. Joining next fleet")
+                    break
+                elif p < top.p and t > top.t:
+                    # print(f"  Behind and slower. Adding new fleet.")
+                    stack.append(Item(p, t))
+                    break
+                elif p > top.p and t >= top.t:
+                    # print(f"  Ahead and slower. Replacing previous fleet")
+                    stack.pop()
+                elif p > top.p and t < top.t:
+                    # print(f"  Ahead and faster. Moving in front of next fleet")
+                    behind.append(stack.pop())
+                
+
+            if not stack:
+                # print(f"No cars available. Adding new fleet")
+                stack.append(Item(p, t))
+
+            stack.extend(behind)
+
+        return len(stack)
